0b9f84701f6401e0ca3e191c0e6f8705a952d8e8
nivethaaarthi98 <30368440+nivethaaarthi98@users.noreply.github.com>
2017-07-25 11:27:45 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+char str1[30],str2[30];
+int i;
+printf("enter two strings:");
+gets(str1);
+gets(str2);
+i=0;
+while(str1[i]==str2[i]&&i++;
+if(str1[i]>str2[i])
+printf("str1>str2");
+elseif(str1[i]<str2[i])
+printf("str1<str2");
+else
+printf("str1=str2");
+return (0);
+}
